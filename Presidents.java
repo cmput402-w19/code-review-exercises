603a7c67bd9c7b18bf3316547df5826f7dc78074
snadi <nadi@ualberta.ca>
2019-01-14 14:05:50 -0700

    Add code review example 1

---
+++ Presidents.java
@@ -0,0 +1,12 @@
+import java.lang.String;
+
+public class Test {
+
+    public static void main(String args[]){
+        String p[] = {"George Washington", "Abe Lincoln", "Herbert Hoover"};
+
+        for (int i = 0; i < 4; i++){
+            System.out.println("President: " + p[i]);
+        }
+    }
+}
